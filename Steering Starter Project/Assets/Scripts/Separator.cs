3a4cea333060bffaf0d51dd1f9f66c5efca224da
6185jcaron <66760646+Ace-03@users.noreply.github.com>
2025-01-23 16:14:16 -0700

    added new AI

---
+++ Steering Starter Project/Assets/Scripts/Separator.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Separator : Kinematic
+{
+    Separation myMoveType;
+    Face mySeekRotateType;
+    LookWhereGoing myFleeRotateType;
+    public Kinematic[] myTargets;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        myMoveType = new Separation();
+        myMoveType.character = this;
+        myMoveType.targets = myTargets;
+        //myMoveType.flee = flee;
+    }
+
+    // Update is called once per frame
+    protected override void Update()
+    {
+        steeringUpdate = new SteeringOutput();
+        steeringUpdate.linear = myMoveType.getSteering().linear;
+    }
+}
